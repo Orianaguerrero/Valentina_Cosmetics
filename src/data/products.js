a8ec8b0d1654cbbb485ae1d0adedbd6c4618a411
Orianaguerrero <guerrerooriana18@gmail.com>
2023-05-05 23:26:50 -0400

    agregando base de datos de usuarios y productos en json

---
+++ src/data/products.js
@@ -0,0 +1,50 @@
+[
+    {
+        id: 1,
+        name: "Lash sensational máscara de pestañas sky high",
+        description: "Máscara de pestañas de volumen y alargamiento Sky High para un impacto espectacular en las pestañas desde todos los ángulos. La máscara de pestañas de larga duración ofrece un volumen completo y una longitud ilimitada.",
+        image: "maybelline-lash.webp",
+        category: "",
+        price: 10.99
+    },
+    {
+        id: 2,
+        name: "Perfect blush too faced",
+        description: "Tres swatches perfecta de ultraflattering Blush le permiten a cualquiera Swirl los colores juntos y tulipa de aplicar o usar más de uno para un personalizado Flush o resaltar. Este Blush fue desarrollado con una fácil aplicación y un no-fail paleta de colores a tomar las conjeturas de crear un dulce, romántico Flush",
+        image: "foofaced-blush.jpg",
+        category: "",
+        price: 8.99
+    },
+    {
+        id: 3,
+        name: "Base liquida ifallible l´oreal",
+        description: "Base líquida liviana de cobertura alta. Duración de hasta 24 horas que mantiene la piel hidrata y permite su transpiración mientras el acabado se mantiene intacto. Resistente al sudor, al agua y a la transferencia. Está formulada con pigmentos que unifican el tono de la piel.",
+        image: "loreal-base.jpg",
+        category: "",
+        price: 25.99
+    },
+    {
+        id: 4,
+        name: "Polvo compacto doble uso divine L´bel",
+        description: "Son unos delicados y deliciosos polvos Compactos doble uso: húmedos como base, ayudan a cubrir imperfecciones y secos como polvos, ayudan a matificar.",
+        image: "loreal-polvo.jpg",
+        category: "",
+        price: 11.99
+    },
+    {
+        id: 5,
+        name: "Labial rouge allure channel",
+        description: "Una barra de labios intensa y luminosa. Una textura ultrafina, fundente y de efecto segunda piel, una paleta de tonos intensos. Un estilo afirmado y audaz.",
+        image: "loreal-polvo.jpg",
+        category: "",
+        price: 24.99 
+    },
+    {
+        id: 6,
+        name: "Benetint benefit",
+        description: "Tinta de labios y mejillas que ofrece un acabado de aspecto natural.",
+        image: "benefit-tint.jpg",
+        category: "",
+        price: 4.99 
+    }
+]
